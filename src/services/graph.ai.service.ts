5f103b067dbdd758c97f54ea94117c06bae89962
Adnan <adnanshakir780@gmail.com>
2026-04-07 20:56:21 +0530

    feat: add LangChain dependencies and implement configuration for API keys
    - Added dependencies for LangChain libraries: @langchain/cohere, @langchain/core, @langchain/google, @langchain/langgraph, @langchain/mistralai, and dotenv.
    - Created a config.ts file to manage API keys for Google, Mistral, and Cohere using dotenv.
    - Implemented graph.ai.service.ts to define the BattleArenaState and its initial state.
    - Developed models.service.ts to initialize models for Google, Mistral, and Cohere with their respective API keys.

---
+++ src/services/graph.ai.service.ts
@@ -0,0 +1,31 @@
+import {
+  StateSchema,
+  MessagesValue,
+  StateGraph,
+  START,
+  END,
+} from "@langchain/langgraph";
+
+type JUDGEMENT = {
+  winner: "solution_1" | "solution_2" | "tie";
+  solution_1_score: number;
+  solution_2_score: number;
+};
+
+type BattleArenaState = {
+  messages: typeof MessagesValue;
+  solution_1: string;
+  solution_2: string;
+    judgement: JUDGEMENT;
+};
+
+const state:BattleArenaState = {
+    messages: MessagesValue,
+    solution_1: "",
+    solution_2: "",
+    judgement: {
+        winner: "tie",
+        solution_1_score: 0,
+        solution_2_score: 0,
+    }
+}
